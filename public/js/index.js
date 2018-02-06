ccf012c187bace82596238057d61671e4a3c26b3
Bastienrc <bastienrc@laposte.net>
2018-02-06 18:51:35 +0100

    Initial commit

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+$(function (){
+  $("#form-pop").popover(); 
+});
+
+$(function() {
+  $('.menu li').click( function() {
+    $(this).addClass('active').siblings().removeClass('active');
+  });
+});
