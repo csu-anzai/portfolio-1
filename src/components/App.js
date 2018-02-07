0cd68c199c823f3ab34f15bc382ccd06207fc3f1
Bastienrc <bastienrc@laposte.net>
2018-02-07 17:33:07 +0100

    Ajout du component React : Citation, à la première page.

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Citation from './Citation'
+import citations from '../data/citations'
+
+class App extends React.Component {
+
+  state = {}
+
+  componentWillMount() {
+    this.genererCitation()
+  }
+
+  genererCitation = e => {
+    const keyArray = Object.keys(citations)
+    const randomKey = keyArray[Math.floor(Math.random() * keyArray.length)]
+    if (this.state.citation === citations[randomKey].citation) {
+      this.genererCitation()
+      return
+    }
+    this.setState(citations[randomKey])
+  }
+
+  render() {
+    return (
+      <div class="citation">
+        <Citation details={this.state} />
+        <button onClick={e => this.genererCitation(e)} >Une autre citation !</button>
+      </div>
+    )
+  }
+}
+
+export default App
