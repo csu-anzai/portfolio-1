199e799b3e9b092226cab1de4d53779368eafe2f
Sebastien Colbert <colbertsebastien@gmail.com>
2019-07-13 22:55:16 +0200

    Intégration de bootstrap en sass et mise a jour du HEAD et de la nav

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+import './custom.scss';
+import 'bootstrap/js/src/collapse.js';
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
-import './index.css'
 
 render(
   <App />,
