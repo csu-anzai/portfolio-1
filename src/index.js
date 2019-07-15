722001a063f6c5be93eb06d7e92875bca0203711
Sebastien Colbert <colbertsebastien@gmail.com>
2019-07-15 18:24:25 +0200

    Ajout de font-awesome et corrections mineurs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './custom.scss';
 import 'bootstrap/js/src/collapse.js';
+import 'fontawesome/index';
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
